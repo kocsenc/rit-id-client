8c48d6fe73de3aec8d064403310fad5715ffba55
Kocsen <kocsenc@gmail.com>
2015-09-10 15:48:04 -0400

    Init yo

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ritIdClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ritIdClientApp
+ */
+angular.module('ritIdClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
